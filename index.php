4e7904992d2182808ea06743d460f23890449141
JEBBOURAY <72884531+JEBBOURAY@users.noreply.github.com>
2026-06-22 20:49:51 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,31 @@
-<?php​
+<?php
 
-​
-echo "Hello World!";​
+echo "Hello World!";
 
-​
-$serverName = getenv('SQL_SERVER_NAME'); // Get server name from environment variables​
 
-$connectionOptions = array(​
+$serverName = getenv('SQL_SERVER_NAME'); // Get server name from environment variables
 
-    "Database" => getenv('SQL_DATABASE_NAME'), // Get database name from environment variables​
+$connectionOptions = array(
 
-    "Uid" => getenv('SQL_UID'),               // Get username from environment variables​
+    "Database" => getenv('SQL_DATABASE_NAME'), // Get database name from environment variables
 
-    "PWD" => getenv('SQL_PWD')                // Get password from environment variables​
+    "Uid" => getenv('SQL_UID'),               // Get username from environment variables
 
-);​
+    "PWD" => getenv('SQL_PWD')                // Get password from environment variables
 
-​
-try {​
+);
 
-    $conn = new PDO("sqlsrv:server=$serverName;Database=".$connectionOptions['Database'], $connectionOptions['Uid'], $connectionOptions['PWD']);​
 
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);​
+try {
 
-    echo "Connected!";​
+    $conn = new PDO("sqlsrv:server=$serverName;Database=".$connectionOptions['Database'], $connectionOptions['Uid'], $connectionOptions['PWD']);
 
-} catch (PDOException $e) {​
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    echo "Connection failed: " . $e->getMessage();​
+    echo "Connected!";
+
+} catch (PDOException $e) {
+
+    echo "Connection failed: " . $e->getMessage();
 
 }
